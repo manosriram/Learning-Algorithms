26f2b228b79456c38f0b2974bc77bf96ca6d8c7c
Mano Sriram <mano.sriram0@gmail.com>
2019-08-18 22:10:57 +0530

    Add Gradient Descent.

--- Linear-Regression/CostFunction.py
+++ Linear-Regression/CostFunction.py
@@ -2,16 +2,6 @@ import pandas as pd
 import numpy as np
 from numpy import ones
 
-df = pd.read_csv("ex1data1.txt", names = ['X', 'y'])
-
-y = df['y']
-m = y.size
-
-X = df.as_matrix(columns = ['X'])
-X = np.append(ones((m,1)), X, axis=1)
-
-theta = np.zeros(2)
-
 def computeCost(X, y, theta):
 	J = 0
 	m = y.size
@@ -20,6 +10,14 @@ def computeCost(X, y, theta):
 	J = (sqrEr) / (2 * m)
 	return J
 
+df = pd.read_csv("ex1data1.txt", names = ['X', 'y'])
+
+y = df['y']
+m = y.size
+
+X = df.as_matrix(columns = ['X'])
+X = np.append(ones((m,1)), X, axis=1)
+theta = np.zeros(2)
 
 cost = computeCost(X, y, theta)
 print(cost)
