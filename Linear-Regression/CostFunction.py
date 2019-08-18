960d9f334c347bb10fdac072fe0897124cb63ff8
Mano Sriram <mano.sriram0@gmail.com>
2019-08-18 13:41:04 +0530

    Add Cost-Function.

--- Linear-Regression/CostFunction.py
+++ Linear-Regression/CostFunction.py
@@ -1,26 +1,25 @@
 import pandas as pd
 import numpy as np
+from numpy import ones
 
+df = pd.read_csv("ex1data1.txt", names = ['X', 'y'])
 
-data = pd.read_csv("./ex1data1.txt", sep=",", header=None)
-m = len(data)
-X = data[0]
+y = df['y']
+m = y.size
+
+X = df.as_matrix(columns = ['X'])
+X = np.append(ones((m,1)), X, axis=1)
+
+theta = np.zeros(2)
+
 def computeCost(X, y, theta):
-	predicted = X * theta
-	actual = y
-	# sqrError = np.power((predicted - actual), 2)
-	print(len(predicted))
-	cost = 1 / (2 * m) * np.sum([1,2,3])
-	return 1
+	J = 0
+	m = y.size
+	h = np.dot(X, theta)
+	sqrEr = np.sum(np.square(h - y))
+	J = (sqrEr) / (2 * m)
+	return J
 
 
-X1 = np.ones((m, 1), dtype="int")
-theta = np.zeros((2,1), dtype="int")
-print(np.dot(X1, theta))
-
-
-theta = np.zeros((2,1), dtype = "int")
-
-y = data[1]
-
-# cost = computeCost(X,y,theta)
+cost = computeCost(X, y, theta)
+print(cost)
